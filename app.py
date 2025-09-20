d132851846d26e06242ebcf23387c5bdc80c67af
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2025-09-20 14:29:36 -0300

    Flask cors para permitir o front e o microsserviço de habits se conectarem.

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask_migrate import Migrate
 from config import Config
 from models.User import db
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Configuração do Flask-CORS
+CORS(app, origins=["http://localhost:3000", "http://localhost:5003"])
 
 db.init_app(app)
 migrate = Migrate(app, db)
